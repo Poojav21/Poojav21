5db72c2130a26c6bc1ccfc42e3514f3f10374d3e
Pooja <vermapooja57764@gmail.com>
2026-09-26 12:27:44 +0530

    Time: 195 ms (20.98%), Space: 20.6 MB (82.49%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,20 +1,59 @@
+import math
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def totalHours(piles, i):
-            total_hours = 0
-            for k in piles:
-                total_hours += math.ceil(k/i)
-            return total_hours
-        res = float('inf')
+        def total_time(m, piles):
+            t = 0
+            for i in range(len(piles)):
+                t += math.ceil(piles[i]/m)
+            return t
         l = 1
         r = max(piles)
+        ans = float('inf')
         while l<=r:
             m = (l+r)//2
-            total_hours = totalHours(piles, m)
-            if total_hours <= h:
-                res = min(res , m)
+            t = total_time(m, piles)
+            if t <= h:
+                ans = min(ans, m)
                 r = m-1
             else:
                 l = m+1
-        return res
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # def totalHours(piles, i):
+        #     total_hours = 0
+        #     for k in piles:
+        #         total_hours += math.ceil(k/i)
+        #     return total_hours
+        # res = float('inf')
+        # l = 1
+        # r = max(piles)
+        # while l<=r:
+        #     m = (l+r)//2
+        #     total_hours = totalHours(piles, m)
+        #     if total_hours <= h:
+        #         res = min(res , m)
+        #         r = m-1
+        #     else:
+        #         l = m+1
+        # return res
         
